70a152b8ee377398ea33bf03130fbcb3734823c8
Ed Stephinson <edward.stephinson@gmail.com>
2023-01-05 14:56:31 +0000

    initial

---
+++ src/index.ts
@@ -0,0 +1,135 @@
+import fetch from "node-fetch";
+
+// Nested keyof utility type
+type NestedKeyOf<T, U = T> = U extends object
+  ? {
+      [K in keyof U]-?: K extends string
+        ? `${K}` | `${K}.${NestedKeyOf<T, U[K]>}`
+        : never;
+    }[keyof U]
+  : never;
+
+type TimeWindow = `${number}${"s" | "m" | "h"}`;
+
+interface GetEventOptions<T> {
+  time_window?: TimeWindow;
+  queryable_path?: NestedKeyOf<T>;
+}
+
+export interface IEvent<T> {
+  id: string;
+  data: T;
+  queryableValue: string;
+  timestamp: string;
+  ack: () => Promise<void>;
+}
+
+class Event<T> implements IEvent<T> {
+  id: string;
+  data: T;
+  queryableValue: string;
+  timestamp: string;
+  client: SailhouseClient;
+  topic: string;
+  subscription: string;
+
+  constructor(
+    event: IEvent<T>,
+    topic: string,
+    subscription: string,
+    client: SailhouseClient,
+  ) {
+    this.id = event.id;
+    this.data = event.data;
+    this.queryableValue = event.queryableValue;
+    this.timestamp = event.timestamp;
+    this.client = client;
+    this.topic = topic;
+    this.subscription = subscription;
+  }
+
+  ack = async (): Promise<void> => {
+    console.log("Acking event", this.id, this.topic, this.subscription);
+    return await this.client.ackEvent(this.topic, this.subscription, this.id);
+  };
+}
+
+export class SailhouseClient {
+  apiKey: string;
+  constructor() {
+    this.apiKey = process.env.SAILHOUSE_API_KEY ?? "";
+  }
+
+  queryEvents = async <T extends unknown>(
+    topic: string,
+    subscription: string,
+    query: string,
+    options: GetEventOptions<T> = {},
+  ): Promise<IEvent<T>[]> => {
+    const path = `https://api.sailhouse.dev/topics/${topic}/subscriptions/${subscription}/events`;
+    const url = new URL(path);
+
+    Object.entries({ ...options, query }).forEach(([key, value]) => {
+      url.searchParams.set(key, value);
+    });
+
+    const results = await fetch(url.toString(), {
+      headers: {
+        Authorization: this.apiKey,
+      },
+    }).then((res) => res.json() as Promise<IEvent<T>[]>);
+    return results.map((event) => new Event(event, topic, subscription, this));
+  };
+
+  getEvents = async <T extends unknown>(
+    topic: string,
+    subscription: string,
+    options: GetEventOptions<T> = {},
+  ): Promise<IEvent<T>[]> => {
+    const path = `https://api.sailhouse.dev/topics/${topic}/subscriptions/${subscription}/events`;
+    const url = new URL(path);
+
+    Object.entries(options).forEach(([key, value]) => {
+      url.searchParams.set(key, value);
+    });
+
+    const results = await fetch(url.toString(), {
+      headers: {
+        Authorization: this.apiKey,
+      },
+    }).then((res) => res.json() as Promise<IEvent<T>[]>);
+
+    return results.map((event) => new Event(event, topic, subscription, this));
+  };
+
+  sendEvent = async <T extends unknown>(
+    topic: string,
+    event: T,
+  ): Promise<void> => {
+    const path = `https://api.sailhouse.dev/topics/${topic}/events`;
+
+    return await fetch(path, {
+      method: "POST",
+      body: JSON.stringify({ data: event }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: this.apiKey,
+      },
+    }).then(() => {});
+  };
+
+  ackEvent = async (
+    topic: string,
+    subscription: string,
+    eventId: string,
+  ): Promise<void> => {
+    const path = `https://api.sailhouse.dev/topics/${topic}/subscriptions/${subscription}/events/${eventId}`;
+
+    return await fetch(path, {
+      method: "POST",
+      headers: {
+        Authorization: this.apiKey,
+      },
+    }).then(() => {});
+  };
+}
